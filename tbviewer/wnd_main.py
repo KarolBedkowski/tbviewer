0b66ea8f318982e2f5ed8de84abee80090dd6f9f
Karol Będkowski <karol.bedkowski@gmail.com>
2020-01-20 20:10:25 +0100

    wndMain clean

--- tbviewer/wnd_main.py
+++ tbviewer/wnd_main.py
@@ -21,7 +21,7 @@ from tkinter import ttk
 from tkinter import tix
 
 from . import map_loader
-from .formatting import format_pos_latlon
+from . import formatting
 
 
 __author__ = "Karol Będkowski"
@@ -41,10 +41,12 @@ class WndMain(tk.Tk):
         self.title("TBViewer")
 
         self._tb_atlas = None
+        # current map image
         self._map_image = None
         self._tiles = {}
         self._last_dir = "."
-        self.canvas_scale = 0
+        # map zoom; scale = 2^zoom
+        self._zoom = 0
 
         self.grid_columnconfigure(2, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -67,13 +69,8 @@ class WndMain(tk.Tk):
                                  relief=tk.SUNKEN)
         self._canvas.grid(column=2, row=0, sticky=tk.NSEW)
 
-        self._status = tk.Label(self, text="", bd=1, relief=tk.SUNKEN,
-                                anchor=tk.W)
-        self._status.grid(column=0, row=2, sticky=tk.EW)
-        self._status_scale = tk.Label(self, text="", bd=1, relief=tk.SUNKEN,
-                                      anchor=tk.W)
-        self._status_scale.grid(column=1, row=2, columnspan=2, sticky=tk.EW)
-
+        self._create_status_bar(self).grid(column=0, columnspan=3, row=2,
+                                           sticky=tk.EW)
         ttk.Sizegrip(self).grid(column=3, row=2, sticky=tk.SE)
 
         if fname:
@@ -93,11 +90,24 @@ class WndMain(tk.Tk):
         self.geometry("1024x768")
         self.lift()
 
+    def _create_status_bar(self, parent):
+        frame = tk.Frame(parent)
+        frame.grid_columnconfigure(3, weight=1)
+        self._status_lon = tk.Label(frame, text="", bd=1, relief=tk.SUNKEN)
+        self._status_lon.grid(column=0, row=0, ipadx=3)
+        self._status_lat = tk.Label(frame, text="", bd=1, relief=tk.SUNKEN)
+        self._status_lat.grid(column=1, row=0, ipadx=3)
+        self._status_scale = tk.Label(frame, text="", bd=1, relief=tk.SUNKEN)
+        self._status_scale.grid(column=2, row=0, ipadx=3)
+        self._status_text = tk.Label(frame, text="", bd=1, anchor=tk.E)
+        self._status_text.grid(column=3, row=0, ipadx=3, sticky=tk.W)
+        return frame
+
     def _build_menu(self):
         menubar = tk.Menu(self)
         self.config(menu=menubar)
         file_menu = tk.Menu(menubar, tearoff=False)
-        file_menu.add_command(label="Open", command=self._open_file)
+        file_menu.add_command(label="Open...", command=self._open_file)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.quit)
         menubar.add_cascade(label="File", menu=file_menu)
@@ -114,6 +124,8 @@ class WndMain(tk.Tk):
         self.focus_set()
 
     def _load(self, fname):
+        self._status_text.config(text=f"Loading {fname}...")
+        self._status_text.update_idletasks()
         self._canvas.delete('img')
         for iid in self._tree.get_children():
             self._tree.delete(iid)
@@ -127,7 +139,13 @@ class WndMain(tk.Tk):
 
         self._clear_tile_cache()
 
-        file_type = map_loader.check_file_type(fname)
+        try:
+            file_type = map_loader.check_file_type(fname)
+        except IOError as err:
+            messagebox.showerror("Error loading file", f"Read file {err}")
+            self._status_text.config(text="Load error...")
+            return
+
         _LOG.info('Loading %s, %r', fname, file_type)
         if file_type in ('atlas', 'tar-atlas'):
             self._tb_atlas = map_loader.Atlas(fname)
@@ -137,6 +155,7 @@ class WndMain(tk.Tk):
             messagebox.showerror(
                 "Error loading file",
                 "Invalid file - should be .map or .tar or .tba")
+            self._status_text.config(text="Load error...")
             return
 
         idx = 0
@@ -156,6 +175,7 @@ class WndMain(tk.Tk):
             if tree_maps:
                 self._tree.selection_set(tree_maps[0])
                 self._on_tree_click(None, item=tree_maps[0])
+        self._status_text.config(text="")
 
     def _load_map(self, filename):
         _LOG.info("_load_map %s", filename)
@@ -164,14 +184,15 @@ class WndMain(tk.Tk):
 
         try:
             self._map_image = map_loader.Map(filename)
-            self.canvas_scale = 0
+            self._zoom = 0
             self._canvas.config(scrollregion=(0, 0, self._map_image.width,
                                               self._map_image.height))
         except map_loader.InvalidFileException as err:
-            messagebox.showerror(
-                "Error loading file",
-                "Invalid file: {}".format(err))
-
+            messagebox.showerror("Error loading file",
+                                 f"Invalid file: {err}")
+        except IOError as err:
+            messagebox.showerror("Error loading file",
+                                 f"Open file error: {err}")
         self._clear_tile_cache()
         self._draw_tiles(True)
 
@@ -198,70 +219,71 @@ class WndMain(tk.Tk):
         self._draw_tiles()
 
     def _canvas_mouse_motion(self, event):
-        if not self._map_image:
-            self._status.config(text='')
-            self._status.update_idletasks()
-            return
-        scale = 2 ** self.canvas_scale
-        x = self._canvas.canvasx(event.x) / scale
-        y = self._canvas.canvasy(event.y) / scale
-        lat, lon = self._map_image.map_data.xy2latlon(x, y)
-        self._status.config(text=format_pos_latlon(lat, lon))
-        self._status.update_idletasks()
-        self._status_scale.config(text="scale: {:0.2f}x".format(scale))
+        lat_txt = lon_txt = ""
+        if self._map_image:
+            scale = 2 ** self._zoom
+            x = self._canvas.canvasx(event.x) / scale
+            y = self._canvas.canvasy(event.y) / scale
+            lat, lon = self._map_image.map_data.xy2latlon(x, y)
+            lat_txt = formatting.format_pos(lat)
+            lon_txt = formatting.format_pos(lon, True)
+        self._status_lat.config(text=lat_txt)
+        self._status_lon.config(text=lon_txt)
+        self._status_lat.update_idletasks()
+        self._status_lon.update_idletasks()
+        self._status_scale.config(text=" {:0.2f}x ".format(scale))
 
     def _canvas_mouse_wheel(self, event):
-        _LOG.debug("event: %r", event)
-        if event.num == 5 or event.delta == -120 and self.canvas_scale > -5:
-            self.canvas_scale -= 1
-        elif event.num == 4 or event.delta == 120 and self.canvas_scale < 5:
-            self.canvas_scale += 1
+        if event.num == 5 or event.delta == -120 and self._zoom > -5:
+            self._zoom -= 1
+        elif event.num == 4 or event.delta == 120 and self._zoom < 5:
+            self._zoom += 1
         self._clear_tile_cache()
 
-        scale = 2.0 ** self.canvas_scale
-        width = int(self._map_image.width * scale)
-        height = int(self._map_image.height * scale)
-        self._canvas.config(scrollregion=(0, 0, width, height))
+        scale = 2.0 ** self._zoom
+        map_width = int(self._map_image.width * scale)
+        map_height = int(self._map_image.height * scale)
+        self._canvas.config(scrollregion=(0, 0, map_width, map_height))
         self._draw_tiles(True)
+        # refresh status
         self._canvas_mouse_motion(event)
 
     def _draw_tiles(self, clear=False):
         if not self._map_image:
             return
         canvas = self._canvas
-        scale = 2.0 ** self.canvas_scale
-        _LOG.debug("scale: %r", scale)
+        scale = 2.0 ** self._zoom
         mapset_get_tile = self._map_image.get_tile
         tile_width = self._map_image.tile_width
         tile_height = self._map_image.tile_height
-
-        tile_show_width = int(tile_width * scale)
-        tile_show_height = int(tile_height * scale)
+        scaled_tile_width = int(tile_width * scale)
+        scaled_tile_height = int(tile_height * scale)
+        # canvas visible area
         visible_x0 = max(canvas.canvasx(0), 0)
         visible_y0 = max(canvas.canvasy(0), 0)
         visible_x1 = canvas.winfo_width()
         visible_y1 = canvas.winfo_height()
 
-        tile_start_x = int(max(visible_x0 // tile_show_width, 0))
-        tile_start_y = int(max(visible_y0 // tile_show_height, 0))
-        tiles_x = int(visible_x1 // tile_show_width) + 2
-        tiles_y = int(visible_y1 // tile_show_height) + 2
+        tile_start_x = int(max(visible_x0 // scaled_tile_width, 0))
+        tile_start_y = int(max(visible_y0 // scaled_tile_height, 0))
+        tiles_x = int(visible_x1 // scaled_tile_width) + 2
+        tiles_y = int(visible_y1 // scaled_tile_height) + 2
 
         new_tile_list = {}
-        for tx in range(tile_start_x, tile_start_x + tiles_x):
-            tx *= tile_width
-            for ty in range(tile_start_y, tile_start_y + tiles_y):
-                ty *= tile_height
+        for txi in range(tile_start_x, tile_start_x + tiles_x):
+            tx = tile_width * txi
+            for tyi in range(tile_start_y, tile_start_y + tiles_y):
+                ty = tile_height * tyi
                 iidimg = self._tiles.get((tx, ty))
                 if iidimg:
                     new_tile_list[(tx, ty)] = iidimg
-                else:
-                    img = mapset_get_tile(tx, ty, scale)
-                    if img:
-                        iid = canvas.create_image(
-                            tx * scale, ty * scale,
-                            image=img, anchor=tk.NW)
-                        new_tile_list[(tx, ty)] = iid, img
+                    continue
+
+                img = mapset_get_tile(tx, ty, scale)
+                if img:
+                    iid = canvas.create_image(tx * scale, ty * scale,
+                                              image=img, anchor=tk.NW)
+                    new_tile_list[(tx, ty)] = iid, img
 
         # remove unused tiles
         for txty, (iid, _) in self._tiles.items():
@@ -273,7 +295,3 @@ class WndMain(tk.Tk):
         for (iid, _) in self._tiles.values():
             self._canvas.delete(iid)
         self._tiles.clear()
-
-
-def _round_to(x, y):
-    return int((x // y) * y)
